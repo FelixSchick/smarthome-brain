4d5e2a944df9f2aa58b6bc75cd638322439fd717
felixschick <schick.felix112@gmail.com>
2025-03-15 11:59:30 +0100

    add: init

---
+++ src/main/java/de/felixschick/smarthomerestapi/SmarthomeRestapiApplication.java
@@ -0,0 +1,13 @@
+package de.felixschick.smarthomerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarthomeRestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmarthomeRestapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/felixschick/smarthomerestapi/SmarthomeRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.felixschick.smarthomerestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarthomeRestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
