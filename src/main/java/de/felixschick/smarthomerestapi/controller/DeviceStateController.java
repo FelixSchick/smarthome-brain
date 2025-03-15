a7736a5f1c28cccaf17da9958fa7d3261e62c6f0
felixschick <schick.felix112@gmail.com>
2025-03-15 17:12:58 +0100

    feat: security

--- src/main/java/de/felixschick/smarthomerestapi/controller/DeviceStateController.java
+++ src/main/java/de/felixschick/smarthomerestapi/controller/DeviceStateController.java
@@ -3,6 +3,8 @@ package de.felixschick.smarthomerestapi.controller;
 import de.felixschick.smarthomerestapi.sources.SmarthomeBridgeAPIFactory;
 import de.felixschick.smarthomerestapi.sources.interfaces.iSmarthomeBridgeAPISource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -34,13 +36,18 @@ public class DeviceStateController {
     }
 
     @PostMapping("/toggle")
-    public void toggleDeviceState(@RequestParam String source, @RequestParam String deviceID) {
+    public ResponseEntity toggleDeviceState(@RequestParam String source, @RequestParam String deviceID) {
         iSmarthomeBridgeAPISource apiSource = smarthomeBridgeAPIFactory.getSource(source);
+
+        if (apiSource == null) return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Source not found");
+        if (apiSource.getDeviceState(deviceID) == null) ResponseEntity.status(HttpStatus.NOT_FOUND).body("Device not found");
+
         String currentState = apiSource.getDeviceState(deviceID);
         Boolean state = Boolean.parseBoolean(currentState);
         if (state) {
             apiSource.setDeviceState(deviceID, "false");
         } else
             apiSource.setDeviceState(deviceID, "true");
+        return ResponseEntity.status(HttpStatus.OK).body("State toggled new value: " + !state);
     }
 }
---
+++ src/main/java/de/felixschick/smarthomerestapi/security/SecurityConfig.java
@@ -0,0 +1,57 @@
+package de.felixschick.smarthomerestapi.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Value("${api.username}")
+    private String username;
+
+    @Value("${api.password}")
+    private String password;
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withUsername(username)
+                .password(passwordEncoder().encode(password))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                        authorizationManagerRequestMatcherRegistry
+                                .requestMatchers("/api/**").hasRole("USER")
+                                .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(sessionManagementConfigurer ->
+                        sessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
+
+        return http.build();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
